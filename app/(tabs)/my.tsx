077f0b1c8706828342e8f418e104b32cda18a2d9
potatojoayo <potatojoayo@gmail.com>
2025-09-29 16:04:52 +0900

    틀 완료

--- app/(tabs)/my.tsx
+++ app/(tabs)/my.tsx
@@ -1,12 +1,21 @@
 import TabPageWrapper from "@/components/common/tab-page-wrapper";
+import { router } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Settings() {
   return (
     <TabPageWrapper>
-      <View className="h-[200vh] pt-14">
-        <Text className="text-white text-xl px-3 py-1">Settings</Text>
+      <View className="h-[200vh] pt-14 px-4">
+        <TouchableOpacity
+          className="bg-foreground rounded-full p-4"
+          activeOpacity={0.9}
+          onPress={() => {
+            router.push("/login");
+          }}
+        >
+          <Text className="text-background font-bold text-center">로그인</Text>
+        </TouchableOpacity>
       </View>
     </TabPageWrapper>
   );
